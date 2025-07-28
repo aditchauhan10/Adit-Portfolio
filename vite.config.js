2e3656f16438d07eb7e27d936079172f316a3f02
aditchauhan10 <aditchauhan2012@gmail.com>
2025-07-28 07:52:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Adit-Portfolio/', // 👈 This is important for GitHub Pages to work
-});
+})
